3844b2084c951701076af267b948e70069c426e3
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-05-12 15:32:32 -0700

    2/3 landing page implemented

--- client/src/home/home.js
+++ client/src/home/home.js
@@ -1,7 +1,7 @@
 import './home.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -11,12 +11,30 @@ let navigate = useNavigate();
   return (
     <Container>
         <div className="index_page">
-          <h1 data-aos="fade-in" className="title">&nbsp;ADHD GRANDE Crowdfunding&nbsp;</h1>
+          <h1 data-aos="fade-in"  data-aos-duration="1200"className="title">&nbsp;ADHD GRANDE Crowdfunding&nbsp;</h1>
         </div>
-        <div data-aos="fade-left" className='descriptive_div'>
+        <div data-aos="fade-left" data-aos-duration="1200" className='descriptive_div'>
           <div className='workspace_descriptive' onClick={() => navigate('/about_us')} >
             The Best Platform For Crowdfunding Of Any Kind.
           </div>
+        </div>
+        <div className='influence_campaign_workspace'>
+        <div data-aos="fade-up-right" data-aos-duration="1200" className="influence_campaign">
+            <p>Intereseted in opening a campaign?</p>
+            <p className='hide'>Open one NOW:</p>
+            <Button className='btn-redirect' variant='dark' onClick={() => navigate('/open_campaign')}>
+              <strong>New Campaign</strong>
+            </Button>
+        </div>
+        </div>
+        <div className='influence_doante_workspace'>
+        <div data-aos="fade-up-left" data-aos-duration="1200" className="influence_donate">
+            <p>Or do <strong>YOU</strong> want to <strong>DONATE</strong> to a <strong>CAMPAIGN</strong> ?</p>
+            <p className='hide'>Do <strong>IT</strong> right <strong>NOW</strong>:</p>
+            <Button className='btn-redirect' variant='dark' onClick={() => navigate('/campaigns')}>
+              <strong>Campaigns</strong>
+          </Button>
+        </div>
         </div>
     </Container>
   );
