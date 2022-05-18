ec485e439a394a9d9184429211f550ce8160b831
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-05-18 09:54:17 -0700

    opinion workspace for campaign done

--- client/src/campaigns/campaignDetails.js
+++ client/src/campaigns/campaignDetails.js
@@ -23,89 +23,133 @@ export default function CampaignDetails() {
     useEffect(() => {
         getCampaignData();
     }, [])
-    const type = "radio";
 
     return (
         <>
-        <Container className="detail-workspace">
-            <div className="camapign-details">
-                <h2>Title</h2>
-                <img className="photo" src={photo} width="90%"></img>
-                <br />
-                <strong>
-                    Target: 1,000,000 euro
-                </strong>
-                <div className="bar">
-                    <ProgressBar animated variant="success" now={50} />
-                </div>
-                <br />
-                <h2>
-                    About this Campaign:
-                </h2>
-                <div className="description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Condimentum id venenatis a condimentum vitae sapien. Diam phasellus vestibulum lorem sed risus ultricies.
-                    Mi eget mauris pharetra et. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
-                    Quam quisque id diam vel quam elementum pulvinar etiam. Arcu bibendum at varius vel pharetra vel.
-                    Aliquet risus feugiat in ante metus. Eu non diam phasellus vestibulum.
-                    Neque ornare aenean euismod elementum nisi quis eleifend quam.
-                    Lectus proin nibh nisl condimentum id. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien.
-                    Pellentesque sit amet porttitor eget. Quis viverra nibh cras pulvinar mattis nunc sed blandit libero.
-                    Justo eget magna fermentum iaculis eu non. Volutpat ac tincidunt vitae semper quis lectus.
-                    Ultricies lacus sed turpis tincidunt id aliquet risus. Augue mauris augue neque gravida in.
-                </div>
-            </div>
-            <div className="campaign-payment">
-                <h1>Payment</h1>
-                <Form>
-                    <div className="mb-3">
-                        {possibleAmount.map((amaount, index) => {
-                            return (
-                                <Form.Check
-                                    key={index}
-                                    label={amaount}
-                                    name="payment-value"
-                                    value={amaount}
-                                    type={type}
-                                    id={`inline-${type}-${index + 1}`}
-                                />);
-                        })}
+            <Container >
+                <div className="detail-workspace">
+                    <div className="camapign-details">
+                        <h2>Title</h2>
+                        <img className="photo" src={photo} width="90%"></img>
+                        <br />
+                        <strong>
+                            Target: 1,000,000 euro
+                        </strong>
+                        <div className="bar">
+                            <ProgressBar animated variant="success" now={50} />
+                        </div>
+                        <br />
+                        <strong>
+                            Current Value: ...
+                        </strong>
+                        <br />
+                        <h2>
+                            About this Campaign:
+                        </h2>
+                        <div className="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Condimentum id venenatis a condimentum vitae sapien. Diam phasellus vestibulum lorem sed risus ultricies.
+                            Mi eget mauris pharetra et. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+                            Quam quisque id diam vel quam elementum pulvinar etiam. Arcu bibendum at varius vel pharetra vel.
+                            Aliquet risus feugiat in ante metus. Eu non diam phasellus vestibulum.
+                            Neque ornare aenean euismod elementum nisi quis eleifend quam.
+                            Lectus proin nibh nisl condimentum id. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien.
+                            Pellentesque sit amet porttitor eget. Quis viverra nibh cras pulvinar mattis nunc sed blandit libero.
+                            Justo eget magna fermentum iaculis eu non. Volutpat ac tincidunt vitae semper quis lectus.
+                            Ultricies lacus sed turpis tincidunt id aliquet risus. Augue mauris augue neque gravida in.
+                        </div>
                     </div>
-                </Form>
-                <Button variant="dark">Payment</Button>
-                <h4>
-                    Payment History:
-                </h4>
-                <div className="payment-history">
-                    <Card className="history">
-                        <Card.Body>
-                            <blockquote className="blockquote mb-0">
-                                <h5>
-                                    Cineva payed: atat +
-                                </h5>
-                            </blockquote>
-                            <small>12-2-2022</small>
-                        </Card.Body>
-                    </Card>
-                    <Card className="history">
-                        <Card.Body>
-                            <blockquote className="blockquote mb-0">
-                                <h5>
-                                    Cineva payed: atat +
-                                </h5>
-                            </blockquote>
-                            <small>12-2-2022</small>
-                        </Card.Body>
-                    </Card>
-
+                    <div className="campaign-payment">
+                        <h1>Payment</h1>
+                        <Form>
+                            <div className="mb-3">
+                                {possibleAmount.map((amaount, index) => {
+                                    return (
+                                        <Form.Check
+                                            key={index}
+                                            label={amaount}
+                                            name="payment-value"
+                                            value={amaount}
+                                            type="radio"
+                                            id={`inline-radio-${index + 1}`}
+                                        />);
+                                })}
+                            </div>
+                        </Form>
+                        <Button variant="dark">Payment</Button>
+                        <h4>
+                            Payment History:
+                        </h4>
+                        <div className="payment-history">
+                            <Card className="history">
+                                <Card.Body>
+                                    <blockquote className="blockquote mb-0">
+                                        <h5>
+                                            Cineva payed: atat +
+                                        </h5>
+                                    </blockquote>
+                                    <small>12-2-2022</small>
+                                </Card.Body>
+                            </Card>
+                            <Card className="history">
+                                <Card.Body>
+                                    <blockquote className="blockquote mb-0">
+                                        <h5>
+                                            Cineva payed: atat +
+                                        </h5>
+                                    </blockquote>
+                                    <small>12-2-2022</small>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        
-        </Container>
-            <div className="opinion-workspace">
-            Opinions
-        </div>
+                <div className="opinion-workspace">
+                    <h2 className="opinion-title">
+                    Opinions:
+                    </h2>
+                    <Card className="opinion-card">
+                        <Card.Body>
+                            <blockquote className="blockquote mb-0">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                                    erat a ante.
+                                </p>
+                                <footer className="blockquote-footer">
+                                    Someone famous in <cite title="Source Title">Source Title</cite>
+                                </footer>
+                            </blockquote>
+                        </Card.Body>
+                    </Card>
+                    <Card className="opinion-card">
+                        <Card.Body>
+                            <blockquote className="blockquote mb-0">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                                    erat a ante.
+                                </p>
+                                <footer className="blockquote-footer">
+                                    Someone famous in <cite title="Source Title">Source Title</cite>
+                                </footer>
+                            </blockquote>
+                        </Card.Body>
+                    </Card>
+                    <Card className="opinion-card">
+                        <Card.Body>
+                            <blockquote className="blockquote mb-0">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                                    erat a ante.
+                                </p>
+                                <footer className="blockquote-footer">
+                                    Someone famous in <cite title="Source Title">Source Title</cite>
+                                </footer>
+                            </blockquote>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </Container>
         </>
     );
 }
