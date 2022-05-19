2089992ccb490ef09e827b98b1d0b451940b575d
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-05-19 11:07:10 -0700

    opinion delete button and location added

--- client/src/opinion/opinionCard.js
+++ client/src/opinion/opinionCard.js
@@ -1,21 +1,47 @@
-import { Card } from "react-bootstrap";
+import axios from "axios";
+import { Card, Button } from "react-bootstrap";
+import { BASE_API_URL } from "../App";
 
 
 
-export default function OpinionCard(props){
+export default function OpinionCard(props) {
 
-    return(
-        <Card className="opinion-card">
-                        <Card.Body>
-                            <blockquote className="blockquote mb-0">
-                                <p>
-                                    {props.data.description }
-                                </p>
-                                <footer className="blockquote-footer">
-                                    Posted by:  <cite title="Source Title">ADHD</cite>
-                                </footer>
-                            </blockquote>
-                        </Card.Body>
-                    </Card>
+
+    async function deleteOpinion(opinionId) {
+        await axios.delete(BASE_API_URL + "opinions/delete-opinion/" + opinionId)
+            .then(showStatus(opinionId));
+    }
+
+    
+    function showStatus(opinionId) {
+        document.querySelector(`.opinion-card[data-opinion-id="${opinionId}"]`).remove();
+        let statusDiv = document.getElementById(opinionId);
+        statusDiv.style = "display: block;";
+        setTimeout(() => {
+            statusDiv.remove();
+        }, 5000);
+    };
+    
+    return (
+        <>
+            <div className="delete-status" id={props.data.id}>
+                Delete Successful!
+            </div>
+            <Card className="opinion-card" data-opinion-id={props.data.id}>
+                <Card.Header className="opinion-card-head">
+                    <Button onClick={() => { deleteOpinion(props.data.id) }} variant="light"> X </Button>
+                </Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {props.data.description}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Posted by:  <cite title="Source Title">ADHD</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+        </>
     );
 };
