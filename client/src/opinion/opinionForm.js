b7cb7327c131b38e3e42fabf9fdc672e409266d0
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-05-19 19:58:19 -0700

    opinion form validation added

--- client/src/opinion/opinionForm.js
+++ client/src/opinion/opinionForm.js
@@ -11,22 +11,57 @@ export default function OpinionForm() {
     const { id } = useParams();
 
     async function addOpinion() {
-        const description = document.getElementById("description").value;
-        await axios.post(BASE_API_URL + "opinions/add-opinion/" + id, {
-            "description": description
-        })
-            .then((response) => {
-                console.log(response);
+        if (validateSubmit(document.getElementById("description"))) {
+            const description = document.getElementById("description").value;
+            await axios.post(BASE_API_URL + "opinions/add-opinion/" + id, {
+                "description": description
             })
-            .finally(navigate("/campaigns/campaign/"+id));
+                .then((response) => {
+                    console.log(response);
+                })
+                .finally(navigate("/campaigns/campaign/" + id));
+        }
+    };
+
+    function validateField(field) {
+        if (field.value.length <= 3){
+            field.style = "boder: solid 1px red;"
+            field.parentNode.parentNode.children[1].innerText = "Minimun 4 chars!";
+            field.parentNode.parentNode.children[1].style = "display: block;";
+    }else{
+        field.style = "boder: solid 1px black;"
+            field.parentNode.parentNode.children[1].style = "display: none;";
+    }
+}
+
+    function validateSubmit(field){
+        if (field.value.length <= 3){
+            field.style = "boder: solid 1px red;"
+            field.parentNode.parentNode.children[1].innerText = "*Required to proceed!";
+            field.parentNode.parentNode.children[1].style = "display: block;";
+            return false;
+        }else{
+            return true;
+        };
     };
 
     return (
         <div className="opinion-form-workspace">
             <form className="opinion-form">
                 <label>Opinion Description:
-                    <input id="description" type="text" placeholder="Enter your opinion here..." required autoFocus />
-                </label><br />
+                    <textarea
+                        rows="5"
+                        cols="80"
+                        onChange={() => { validateField(document.querySelector("#description")) }}
+                        id="description"
+                        type="text"
+                        placeholder="Enter your opinion here..."
+                        required
+                        autoFocus >
+                    </textarea>
+                </label>
+                <div className="opinion-warning"></div><br />
+
                 <Button variant="dark" onClick={addOpinion}>
                     Submit
                 </Button>
